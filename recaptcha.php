8d42c8437874e2ffe634062a3a53101d5a50ba28
bofh16 <bofh@bofh.bg>
2017-11-21 19:10:01 +0100

    Rewrite of the 2011 original SM CAPTCHA plugin to support recaptchav2

---
+++ recaptcha.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+  * SquirrelMail CAPTCHA Plugin reCAPTCHA Backend
+  * Copyright (c) 2007-2011 Paul Lesniewski <paul@squirrelmail.org>
+  * Licensed under the GNU GPL. For full terms see the file COPYING.
+  *
+  * @package plugins
+  * @subpackage captcha
+  *
+  */
+
+
+
+function recaptcha_check_configuration()
+{
+	global $recaptcha_public_key, $recaptcha_private_key;
+
+	if (empty($recaptcha_public_key) || empty($recaptcha_private_key))
+	{
+		do_err('CAPTCHA plugin reCAPTCHA backend is missing a reCAPTCHA key.  Please see https://www.google.com/recaptcha/admin/create', FALSE);
+		return TRUE;
+	}
+}
+
+
+
+function recaptcha_show_input_widgets()
+{
+	global $recaptcha_public_key;
+
+	$recaptcha_uri = 'https://www.google.com/recaptcha/api.js';
+
+	$output .="<form action=\"\" method=\"post\">\n"
+		. "<tr><td colspan=\"2\"><br /><center>\n"
+		. "<table width=\"350\">\n"
+		. "<tr><td><center>\n"
+		. "<div class=\"g-recaptcha\" data-sitekey=$recaptcha_public_key></div>\n";
+
+	$output .="<script src=$recaptcha_uri></script>\n";
+
+	return $output;
+}
+
+
+
+function recaptcha_validate_captcha()
+{
+	global $recaptcha_private_key;
+	$response = $_POST['g-recaptcha-response'];
+	$verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$recaptcha_private_key.'&response='.$response);
+
+	$verifyResponse = json_decode($verifyResponse);
+	if ($verifyResponse->success):
+        	return TRUE;
+	else:
+		return FALSE;
+	endif;
+}
