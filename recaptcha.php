0d20e87f8f0de36579a29c974104e63dbbb7b083
bofh16 <bofh@bofh.bg>
2017-11-22 14:28:53 +0100

    Optimization

--- recaptcha.php
+++ recaptcha.php
@@ -14,46 +14,46 @@
 
 function recaptcha_check_configuration()
 {
-	global $recaptcha_public_key, $recaptcha_private_key;
+        global $recaptcha_public_key, $recaptcha_private_key;
 
-	if (empty($recaptcha_public_key) || empty($recaptcha_private_key))
-	{
-		do_err('CAPTCHA plugin reCAPTCHA backend is missing a reCAPTCHA key.  Please see https://www.google.com/recaptcha/admin/create', FALSE);
-		return TRUE;
-	}
+        if (empty($recaptcha_public_key) || empty($recaptcha_private_key))
+        {
+                do_err('CAPTCHA plugin reCAPTCHA backend is missing a reCAPTCHA key.  Please see https://www.google.com/recaptcha/admin/create', FALSE);
+                return TRUE;
+        }
 }
 
 
 
 function recaptcha_show_input_widgets()
 {
-	global $recaptcha_public_key;
+        global $recaptcha_public_key;
 
-	$recaptcha_uri = 'https://www.google.com/recaptcha/api.js';
+        $recaptcha_url = 'https://www.google.com/recaptcha/api.js';
 
-	$output .="<form action=\"\" method=\"post\">\n"
-		. "<tr><td colspan=\"2\"><br /><center>\n"
-		. "<table width=\"350\">\n"
-		. "<tr><td><center>\n"
-		. "<div class=\"g-recaptcha\" data-sitekey=$recaptcha_public_key></div>\n";
+        $output .="<form action=\"\" method=\"post\">\n"
+                . "<tr><td colspan=\"2\"><br /><center>\n"
+                . "<table width=\"350\">\n"
+                . "<tr><td><center>\n"
+                . "<div class=\"g-recaptcha\" data-sitekey=$recaptcha_public_key></div>\n";
+                . "<script src=$recaptcha_url></script>\n"
+                . "</form>\n";
 
-	$output .="<script src=$recaptcha_uri></script>\n";
-
-	return $output;
+        return $output;
 }
 
 
 
 function recaptcha_validate_captcha()
 {
-	global $recaptcha_private_key;
-	$response = $_POST['g-recaptcha-response'];
-	$verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$recaptcha_private_key.'&response='.$response);
+        global $recaptcha_private_key;
+        $response = $_POST['g-recaptcha-response'];
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$recaptcha_private_key.'&response='.$response);
 
-	$verifyResponse = json_decode($verifyResponse);
-	if ($verifyResponse->success):
-        	return TRUE;
-	else:
-		return FALSE;
-	endif;
+        $verifyResponse = json_decode($verifyResponse);
+        if ($verifyResponse->success):
+                return TRUE;
+        else:   
+                return FALSE;
+        endif;
 }
